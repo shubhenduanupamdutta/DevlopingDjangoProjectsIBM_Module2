e60856395d9dac5a453bd9a9895d431ef32fab12
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-08-17 18:39:54 +0530

    Create basic landing page for firstapp

--- myfirstproject/firstapp/views.py
+++ myfirstproject/firstapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpRequest, HttpResponse
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    # Create a simple html page as a string
+    template = "<html>This is your first view</html>"
+    # Return the template as content argument in HTTP response
+    return HttpResponse(content=template)
