ad4a122ddfe296d10d5c86bb8ce5c6c21408d2fb
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-08-17 22:17:35 +0530

    Course list template and view

--- lab3_template/onlinecourse/urls.py
+++ lab3_template/onlinecourse/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path
+# ruff: noqa: RUF005
+
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+
 from . import views
 
-app_name = 'onlinecourse'
-urlpatterns = [
-    # Add path here
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
- + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+app_name = "onlinecourse"
+urlpatterns = (
+    [
+        # Add path here
+        path(route="", view=views.popular_course_list, name="popular_course_list"),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
--- lab3_template/onlinecourse/views.py
+++ lab3_template/onlinecourse/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render, redirect
-from .models import Course, Lesson, Enrollment
+from django.http import Http404, HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views import generic
-from django.http import Http404
+
+from .models import Course, Enrollment, Lesson
+
 
 # Create your views here.
-
+def popular_course_list(request: HttpRequest) -> HttpResponse:
+    context = {}
+    # If the request method is GET
+    if request.method == "GET":
+        # Using the objects model manage to read all course list
+        # and sort them by total_enrollment descending
+        course_list = Course.objects.order_by("-total_enrollment")[:10]
+        # Appen the course list as an entry of context dict
+        context["course_list"] = course_list
+        return render(request, "onlinecourse/course_list.html", context)
+    return HttpResponse("Method not allowed", status=405)
