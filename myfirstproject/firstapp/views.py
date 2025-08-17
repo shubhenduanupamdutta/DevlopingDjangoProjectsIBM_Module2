2d0fd9ad88df8e06039adb9c6ca4d685ab40f3e9
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-08-17 18:48:16 +0530

    A get date endpoint

--- myfirstproject/firstapp/views.py
+++ myfirstproject/firstapp/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+from datetime import date
+
 from django.http import HttpRequest, HttpResponse
 
 
@@ -6,4 +8,10 @@ def index(request: HttpRequest) -> HttpResponse:
     # Create a simple html page as a string
     template = "<html>This is your first view</html>"
     # Return the template as content argument in HTTP response
+    return HttpResponse(content=template)
+
+
+def get_date(request: HttpRequest) -> HttpResponse:
+    today = date.today()
+    template = f"<html>Today's date is {today}</html>"
     return HttpResponse(content=template)
