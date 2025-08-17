92b0a25b193246945e1507ca5202fc8c9665a990
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-08-17 22:51:01 +0530

    Deployment SECRET KEY Issue -fix

--- lab2_template/myproject/settings.py
+++ lab2_template/myproject/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's+skk-ba!=wdg9$f495be9b@z5&0r4-g=c0xe&&^gomc1mg801'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
